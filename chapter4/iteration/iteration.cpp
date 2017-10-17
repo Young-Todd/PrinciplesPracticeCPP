cdbb336da0c7c868debc981bb0de2c695a870e29
Todd <myoung44@vols.utk.edu>
2017-10-17 09:01:08 -0400

    umm

--- chapter4/iteration/iteration.cpp
+++ chapter4/iteration/iteration.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 
 void for_vals(int maximum=5){
-    for (int i=0; i<maximum+1; ++i){
+    for(int i=0; i<maximum+1; ++i){
         cout << i << '\t';
     }
 }
 
 void while_vals(int maximum=5){
     int i = 0;
-    while (i < maximum+1){
+    while(i < maximum+1){
         cout << i << '\t';
         i++;
     }
+}
+
+void range_vals(vector<int> values){
+    for(const auto x : values){
+        cout << x << '\t';
+    } 
 }
 
 int main() {
@@ -22,6 +29,9 @@ int main() {
     for_vals();
     cout << "\nPrinting using a while loop:\n";
     while_vals(10);
+    cout << "\nNow using the range loop:\n";
+    vector<int> *vec = new vector<int>(10);
+    range_vals(*vec);
 
     return 0;
 }
